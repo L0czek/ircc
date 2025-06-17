a39e8cbf5c16a1dd0bc09fda368e59358745d22d
Michał Szaknis <michalszak@gmail.com>
2025-06-17 23:07:49 +0200

    Add main task boiler plate

--- controller/src/ctl.cc
+++ controller/src/ctl.cc
@@ -1,6 +1,19 @@
-#include "board.h"
 #include <cstdio>
 
+#include "board.h"
+#include "cmsis_os2.h"
+#include "config.hpp"
+#include "thread.hpp"
+
+#include "log.hpp"
+
 extern "C" void controller_system_init(const board_config *config) {
-    printf("Hello world\n");
+    os::init(LOG_LEVEL);
+
+    os::info("Booting up\n");
+
+    for (uint32_t i=0;; ++i) {
+        os::info("tick %d\n", i);
+        osDelay(1000);
+    }
 }
